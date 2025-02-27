86af7dd3c45f2d69cb4393af769a196fdb8f0339
Matheus <matheus.pgua@hotmail.com>
2025-02-27 15:19:07 -0300

    Versão 0.7 - ao colocar itens no carrinho de vendas, se o item já foi adicionado anteriormente agora o número de itens é atualizado. Também foi adicionado um botão de remover item para cada item adicionado, atualizando automaticamente o valor final

--- interface/realizar_venda.py
+++ interface/realizar_venda.py
@@ -7,23 +7,38 @@ from produtos import Produtos
 from db_manager import db_manager as db
 from time import sleep
 
+buttons_to_remove = []
+
 if not st.session_state['shopping_cart']:
     shopping_cart = []
 
+#def write_on_screen(itemQuantity, itemName, itemPrice):
+#    st.write(f'{itemQuantity}x {itemName} - R${(float(itemQuantity) * float(itemPrice)):.2f}')
+#    st.write(f'Preço unitário: R${itemPrice}')
+
+#def write_on_screen():
+#    final_price = 0
+#    for item in st.session_state['shopping_cart']:
+#        st.write(f'{item[1]}x {item[0]} - R${(float(item[1]) * float(item[2])):.2f}')
+#        st.write(f'Preço unitário: R${item[2]}')
+#        buttons_to_remove.append([st.button(f'Remover {item[1]}x {item[0]}'), [item[0], item[1], item[2]]])
+#        st.divider()
+#        final_price += float(item[2]) * float(item[1])
+#    return final_price
+
 def add_to_shopping_cart(productName, productQuantity, productPrice):
+    for item in st.session_state['shopping_cart']:
+        if(productName in item):
+            item[1] += productQuantity
+            return True
     st.session_state['shopping_cart'].append([productName, productQuantity, productPrice])
-#    st.session_state['shopping_cart'] = True
-#    st.write(f'Nome do produto: {productName}')
-#    st.write(f'Preço do produto: {productPrice}')
-#    st.write(f'Quantidade do produto: {productQuantity}')
-#    shopping_cart.append([productName, productQuantity, productPrice])
+#    print(st.session_state['shopping_cart'])
 
-#j = 0
+def remove_from_shopping_cart(productName, productQuantity, productPrice):
+    st.session_state['shopping_cart'].remove([productName, productQuantity, productPrice])
+    st.switch_page('interface/realizar_venda.py')
+
 categorias = Categoria().get_categories_list()
-#categorias = db().selectTables(('categoria'), 'categorias')
-#    categorias = Categoria().get_categories_list()
-#    produtos = Produtos().get_product_list()
-
 categories_string_list = []
 
 for category in categorias:
@@ -34,8 +49,6 @@ if (not categories_string_list):
     st.write('Não há categorias ou produtos cadastrados! Primeiramente, cadastre uma categoria, e depois um produto.')
 
 else:
-#    with st.form('register_sale'):
-
     products_column, shopping_column = st.columns(2)
 
     with products_column:
@@ -43,12 +56,10 @@ else:
         buttons = []
         product_keys = {}
 
-
         for i in range (len(tabs)):
             temp_products = []
             with tabs[i]:
                 product_list = Produtos().list_item_by_category(categories_string_list[i])
-#                print(product_list)
                 product_name = []
                 product_price = []
 
@@ -61,49 +72,37 @@ else:
                     product_name[k] += f' - R${product_price[k]:.2f}'
                     st.write(product_name[k])
                     temp_products.append(product_name[k])
-#                    product_keys[product_name[k]] = categories_string_list[i]
-#                    st.write('Escolha a quantidade:')
-#                    st.number_input('Escolha a quantidade',0,99,step=1, key=j)
                     st.number_input('Escolha a quantidade',0,99,step=1, key=f'{product_name[k]}')
-#                    j += 1
                     st.divider()
                 product_keys[categories_string_list[i]] = temp_products
                 buttons.append(st.button(f'Adicionar {categories_string_list[i]}'))
-#        print(st.session_state)
-#        print(f'Teste: {product_keys}')
+
         if(True in buttons):
             for i in range (len(buttons)):
                 if(buttons[i]):
-#                    print(f'product_keys[categories_string_list[i]]: {product_keys[categories_string_list[i]]}')
                     for product_key in product_keys[categories_string_list[i]]:
                         product_name_to_cart, product_price_to_cart = product_key.split('-')
                         product_quantity_to_cart = st.session_state[product_key]
                         if(product_quantity_to_cart>0):
                             add_to_shopping_cart(product_name_to_cart, product_quantity_to_cart, product_price_to_cart[3:])
-#                        print(f'product_key: {product_key}')
-#                        print(f'Nome do Produto: {product_name_to_cart}')
-#                        print(f'Preço do Produto: {float(product_price_to_cart[3:])}')
-#                        print(f'Quantidade: {product_quantity_to_cart}')
-#                    print(f'product_key.split("-"): {product_key.split('-')}')
-#                    print(f'categories_string_list[i]: {categories_string_list[i]}')
-#                    print(f'st.session_state: {st.session_state}')
-#                    print(f'product_keys: {product_keys}')
-    
+
     with shopping_column:
-#        print(st.session_state['shopping_cart'])
         st.subheader(':shopping_trolley: Carrinho')
+        buttons_to_remove = []
         if(not st.session_state['shopping_cart']):
             st.write('Não há produtos no carrinho.')
+
         else:
             final_price = 0
+#            write_on_screen()
+
             for item in st.session_state['shopping_cart']:
-                st.write(f'{item[1]} x {item[0]} - R${(float(item[1]) * float(item[2])):.2f}')
-#                st.write(f'Produto: {item[0]}')
-#                st.write(f'Quantidade: {item[1]}')
+                st.write(f'{item[1]}x {item[0]} - R${(float(item[1]) * float(item[2])):.2f}')
                 st.write(f'Preço unitário: R${item[2]}')
+                buttons_to_remove.append([st.button(f'Remover {item[1]}x {item[0]}'), [item[0], item[1], item[2]]])
                 st.divider()
-#                st.write(f'Valor por itens: {float(item[1]) * float(item[2])}')
                 final_price += float(item[2]) * float(item[1])
+
             st.write(f'Preço total: R${final_price:.2f}')
             finish_sale = st.button('Concluir venda')
             if(finish_sale):
@@ -116,9 +115,15 @@ else:
                     productID_ = Produtos().get_product_id(item[0][:-1])
                     Vendas().add_sale(productID_,item[1],float(item[2])*int(item[1]),funcID,shoppingCartIDs,formatted_sale_datetime)
                 st.success('Venda concluída com sucesso!!')
-                for i in range(5,0,-1):
+                for i in range(3,0,-1):
                     st.toast(f'Retornando ao menu principal em {i}')
                     sleep(1)
                 st.session_state['shopping_cart'] = []
                 st.switch_page('interface/menu_principal.py')
-        
+
+        for i in range (len(buttons_to_remove)):
+            if(True in buttons_to_remove[i]):
+                if(buttons_to_remove[i][0]):
+#                    print(st.session_state)
+                    remove_from_shopping_cart(buttons_to_remove[i][1][0], buttons_to_remove[i][1][1], buttons_to_remove[i][1][2]) # É feito desta forma pois buttons_to_remove possui a seguinte estrutura: [[False, ['ITEM1_NAME', ITEM1_QUANTITY, 'ITEM1_VALUE']], [False, ['ITEM2_NAME', ITEM2_QUANTITY, 'ITEM2_VALUE']], ...]
+#                    write_on_screen()
